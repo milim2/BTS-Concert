fe611da8e4d090f368351164d6ddaa859267a983
milim2 <leemil@sheridancollege.ca>
2019-03-14 18:33:30 -0400

    finish Header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,23 +5,67 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
+
+    state = {
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+    
+    handleScroll = () => {
+        if(window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+        }
+        else {
+            this.setState({
+            headerShow: false
+            })
+        }
+    }
+
+    toggleDrawer = (value) => {
+        this.setState({
+            drawerOpen: value
+        })
+    }
+
     render() {
         return (
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor:'#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f': 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0px'
                 }}
                 >
                 <Toolbar>
                     <div className="header_logo">
-                    <div classNmae="font_righteous header_logo_venue">The Venue</div>
-                    <div classNmae="header_logo_title">Sheridan Events</div>
+                    <div className="font_righteous header_logo_venue">The Venue</div>
+                    <div className="header_logo_title">Sheridan Events</div>
 
                     </div>
+                    <IconButton
+                    aria-label="Menu"
+                    color="inherit"
+                    onClick={()=> this.toggleDrawer(true)}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value)=> this.toggleDrawer(value)}
+                    />
+
                 </Toolbar>
 
             </AppBar>
