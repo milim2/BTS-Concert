25cc6c5dad2853e6634be049b11ead7d8d6c9e59
milim2 <leemil@sheridancollege.ca>
2019-05-01 01:15:23 -0400

    button and percentage

---
+++ src/components/Highlights/Description.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Highlights</h2>
+            <div className="highlight_desc">
+            World famous K-pop band BTS has announced North American Tour dates for its 2019 <br/>
+            Love Yourself:Speak Yourself World Tour.<br/>
+The BTS tour dates 2019 will feature U.S. concerts in the Los Angeles Area (Pasadena), the New York City area (East Rutherford, N.J.) and Chicago.<br/>
+The BTS world tour will then continue with shows in Brazil, the U.K., France and Japan.<br/>
+Interestingly, the BTS Army seems to be getting older. The age demographics shifted from the 2018 Love Yourself tour announcement to this year's announcement. As the BTS fan base grows and gets more diverse, it continues to dominate other K-pop acts as the most popular in the world. Vivid Seats has full data on the BTS Army.
+The K-pop boy band was originally known as the Bangtan Boys and is comprised of members V, J-Hope, RM, Jin, Jimin, Jungkook and Suga. It formed in 2013 and has quickly developed one of the world's most passionate fanbases, the BTS Army. Earlier this year, BTS presented an award at the 2019 GRAMMYs.
+            </div>
+
+            </div>
+            
+        </Fade>
+    );
+};
+
+export default Description;
