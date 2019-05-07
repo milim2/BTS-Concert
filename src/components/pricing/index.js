444dc66fecf50d6f833741a7f8d502c080741383
milim2 <leemil@sheridancollege.ca>
2019-05-06 21:34:29 -0400

    pricing

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MyButton from '../utils/MyButton';
+import Zoom from 'react-reveal/Zoom';
 
 class index extends Component {
 
@@ -6,30 +8,38 @@ class index extends Component {
         prices:[100, 150, 200],
         positions:['Balcony', 'Medium', 'Star'],
         desc: [
-            'Seats directly across from the stage provide the most comfortable looks of the performance. Even upper level sections offer unobstructed sitelines. Just keep in mind that you wont be able to see as much detail from these seats as you would if you sat closer to the stage.',
-            'offer some of the closest views of the performers. For many concerts, floor sections are added or removed. These performer-specific adjustments may alter the floor layout and the views from each floor sectio',
+            'Seats directly across from the stage provide the most comfortable looks of the performance. Even upper level sections offer unobstructed',
+            'offer some of the closest views of the performers. For many concerts, floor sections are added or removed. These performer-specific adjustments',
             'The view is not head-on like on the floor, but many of the seats are closer to the stage, provide better elevation and are actual stadium seats.'
         ],
-        linkto:['http://sales/b', 'http://sales/m', 'http://sales/s' ]
+        linkto:['http://sales/b', 'http://sales/m', 'http://sales/s' ],
+        delay:[500, 0, 500]
     }
 
     showBoxes = () => (
         this.state.prices.map((box, i)=>(
-            <div className="pricing_item">
+            <Zoom delay={this.state.delay[i]} key={i}>
+                <div className="pricing_item">
             <div className="pricing_inner_wrapper">
                 <div className="pricing_title">
                     <span>${this.state.prices[i]}</span>
+                    <span>${this.state.positions[i]}</span>
                 </div>
                 <div className="pricing_description">
-                    description
+                    {this.state.desc[i]}
                 </div>
                 <div className="pricing_buttons">
-                    button
+                    <MyButton 
+                        text="Purchase"
+                        bck="ffa800"
+                        color="#ffffff"
+                        link={this.state.linkto[i]}
+
+                    />
                 </div>
             </div>
-
             </div>
-
+            </Zoom>            
         ))
         )
     
@@ -40,7 +50,7 @@ class index extends Component {
             <h2>Pricing</h2>
 
                 <div className="pricing_wrapper">
-                    {this.showBoxes}
+                    {this.showBoxes()}
                     </div>                
             </div>
             </div>
